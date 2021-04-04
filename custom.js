f64c1b04603eabd8a88a3129944c32da563727e7
ahuy <anhhuyd3v@gmail.com>
2021-04-05 00:15:42 +0700

    first commit

---
+++ custom.js
@@ -0,0 +1,566 @@
+var personData = [
+  {
+    name: "Fidgett",
+    position: "Financial Analyst",
+    office: "Ucuncha",
+    age: 23,
+    startdate: "12/11/2020",
+  },
+  {
+    name: "Crabtree",
+    position: "Desktop Support Technician",
+    office: "Hujia",
+    age: 10,
+    startdate: "5/27/2020",
+  },
+  {
+    name: "Rollinson",
+    position: "Senior Sales Associate",
+    office: "Paobulan",
+    age: 77,
+    startdate: "11/24/2020",
+  },
+  {
+    name: "Zamboniari",
+    position: "Physical Therapy Assistant",
+    office: "Police nad Metují",
+    age: 6,
+    startdate: "2/9/2021",
+  },
+  {
+    name: "Rudderham",
+    position: "Senior Editor",
+    office: "Pervoavgustovskiy",
+    age: 12,
+    startdate: "1/30/2021",
+  },
+  {
+    name: "Boosey",
+    position: "Software Engineer III",
+    office: "Spokoynaya",
+    age: 44,
+    startdate: "12/12/2020",
+  },
+  {
+    name: "Greenhall",
+    position: "Health Coach III",
+    office: "Milotice",
+    age: 96,
+    startdate: "8/14/2020",
+  },
+  {
+    name: "Woakes",
+    position: "Software Consultant",
+    office: "Zengguang",
+    age: 90,
+    startdate: "1/6/2021",
+  },
+  {
+    name: "Kenyam",
+    position: "Quality Control Specialist",
+    office: "Avelinha",
+    age: 20,
+    startdate: "9/30/2020",
+  },
+  {
+    name: "Puxley",
+    position: "Quality Control Specialist",
+    office: "Banjar Pasekan",
+    age: 48,
+    startdate: "10/25/2020",
+  },
+  {
+    name: "Corballis",
+    position: "Junior Executive",
+    office: "Gorzów Wielkopolski",
+    age: 56,
+    startdate: "1/9/2021",
+  },
+  {
+    name: "Render",
+    position: "Design Engineer",
+    office: "Yinchuan",
+    age: 16,
+    startdate: "2/1/2021",
+  },
+  {
+    name: "Sallarie",
+    position: "Data Coordiator",
+    office: "Asadābād",
+    age: 7,
+    startdate: "6/9/2020",
+  },
+  {
+    name: "Hawse",
+    position: "Research Assistant II",
+    office: "Knyaginino",
+    age: 66,
+    startdate: "4/7/2020",
+  },
+  {
+    name: "McCree",
+    position: "Web Developer I",
+    office: "Muleng",
+    age: 53,
+    startdate: "7/16/2020",
+  },
+  {
+    name: "Vyvyan",
+    position: "Sales Representative",
+    office: "Gierałtowice",
+    age: 26,
+    startdate: "11/5/2020",
+  },
+  {
+    name: "Kuhnke",
+    position: "Safety Technician IV",
+    office: "Novo-Nikol’skoye",
+    age: 69,
+    startdate: "2/3/2021",
+  },
+  {
+    name: "Pothbury",
+    position: "Civil Engineer",
+    office: "Villa Carlos Paz",
+    age: 65,
+    startdate: "9/13/2020",
+  },
+  {
+    name: "Ledster",
+    position: "Registered Nurse",
+    office: "Plérin",
+    age: 33,
+    startdate: "10/9/2020",
+  },
+  {
+    name: "Beebis",
+    position: "Assistant Media Planner",
+    office: "Rantau",
+    age: 32,
+    startdate: "5/19/2020",
+  },
+  {
+    name: "Bartoszewski",
+    position: "Account Executive",
+    office: "Borås",
+    age: 86,
+    startdate: "4/7/2020",
+  },
+  {
+    name: "Lanchbery",
+    position: "Community Outreach Specialist",
+    office: "Neyvo-Rudyanka",
+    age: 17,
+    startdate: "9/22/2020",
+  },
+  {
+    name: "McGeechan",
+    position: "Editor",
+    office: "Aquidauana",
+    age: 83,
+    startdate: "4/27/2020",
+  },
+  {
+    name: "Rubrow",
+    position: "Statistician III",
+    office: "Tianya",
+    age: 3,
+    startdate: "4/14/2020",
+  },
+  {
+    name: "Tremouille",
+    position: "Structural Analysis Engineer",
+    office: "Kitango",
+    age: 93,
+    startdate: "1/19/2021",
+  },
+  {
+    name: "Toleman",
+    position: "Tax Accountant",
+    office: "São Sepé",
+    age: 94,
+    startdate: "8/3/2020",
+  },
+  {
+    name: "Klimko",
+    position: "Registered Nurse",
+    office: "Komi",
+    age: 32,
+    startdate: "5/17/2020",
+  },
+  {
+    name: "Shakesby",
+    position: "Web Designer IV",
+    office: "Jablonec nad Jizerou",
+    age: 85,
+    startdate: "2/5/2021",
+  },
+  {
+    name: "Bicknell",
+    position: "Actuary",
+    office: "Yushu",
+    age: 37,
+    startdate: "4/23/2020",
+  },
+  {
+    name: "Imorts",
+    position: "Structural Analysis Engineer",
+    office: "Sonsorol Village",
+    age: 62,
+    startdate: "12/29/2020",
+  },
+  {
+    name: "Mixter",
+    position: "Statistician III",
+    office: "Mwene-Ditu",
+    age: 46,
+    startdate: "2/26/2021",
+  },
+  {
+    name: "Nother",
+    position: "Financial Advisor",
+    office: "Pai do Vento",
+    age: 9,
+    startdate: "2/3/2021",
+  },
+  {
+    name: "Duester",
+    position: "VP Marketing",
+    office: "Varybóbi",
+    age: 28,
+    startdate: "4/26/2020",
+  },
+  {
+    name: "Caddy",
+    position: "Associate Professor",
+    office: "Sueyoshichō-ninokata",
+    age: 87,
+    startdate: "5/13/2020",
+  },
+  {
+    name: "Krollmann",
+    position: "Staff Accountant II",
+    office: "Besançon",
+    age: 74,
+    startdate: "10/18/2020",
+  },
+  {
+    name: "Hogben",
+    position: "Quality Control Specialist",
+    office: "Galapa",
+    age: 11,
+    startdate: "6/4/2020",
+  },
+  {
+    name: "Creasey",
+    position: "Desktop Support Technician",
+    office: "Zarichchya",
+    age: 77,
+    startdate: "3/20/2021",
+  },
+  {
+    name: "McGrae",
+    position: "Administrative Officer",
+    office: "Tamano",
+    age: 10,
+    startdate: "6/13/2020",
+  },
+  {
+    name: "Tomley",
+    position: "Health Coach II",
+    office: "Atlanta",
+    age: 77,
+    startdate: "6/16/2020",
+  },
+  {
+    name: "Tzar",
+    position: "Nurse Practicioner",
+    office: "Admiralteisky",
+    age: 67,
+    startdate: "12/28/2020",
+  },
+  {
+    name: "Pallis",
+    position: "Financial Advisor",
+    office: "Savonlinna",
+    age: 4,
+    startdate: "5/20/2020",
+  },
+  {
+    name: "Trengrove",
+    position: "Analyst Programmer",
+    office: "Wattala",
+    age: 79,
+    startdate: "1/1/2021",
+  },
+  {
+    name: "Cardoo",
+    position: "Director of Sales",
+    office: "Hüremt",
+    age: 49,
+    startdate: "2/17/2021",
+  },
+  {
+    name: "Bourdon",
+    position: "Paralegal",
+    office: "Vilarinho das Cambas",
+    age: 56,
+    startdate: "12/1/2020",
+  },
+  {
+    name: "Fennelow",
+    position: "Marketing Manager",
+    office: "Rukem",
+    age: 21,
+    startdate: "10/8/2020",
+  },
+  {
+    name: "Samwayes",
+    position: "Product Engineer",
+    office: "Futian",
+    age: 23,
+    startdate: "8/18/2020",
+  },
+  {
+    name: "Harroll",
+    position: "Teacher",
+    office: "Springbok",
+    age: 26,
+    startdate: "6/22/2020",
+  },
+  {
+    name: "Croxford",
+    position: "Assistant Professor",
+    office: "Srbica",
+    age: 64,
+    startdate: "7/24/2020",
+  },
+  {
+    name: "Bucher",
+    position: "Assistant Professor",
+    office: "Cuiabá",
+    age: 87,
+    startdate: "1/31/2021",
+  },
+  {
+    name: "McConway",
+    position: "Nurse",
+    office: "Wangchang",
+    age: 44,
+    startdate: "8/25/2020",
+  },
+  {
+    name: "Gettens",
+    position: "Media Manager II",
+    office: "Ngulahan",
+    age: 10,
+    startdate: "5/7/2020",
+  },
+  {
+    name: "Blofeld",
+    position: "Compensation Analyst",
+    office: "Canis",
+    age: 29,
+    startdate: "4/14/2020",
+  },
+  {
+    name: "Arrighetti",
+    position: "Accounting Assistant II",
+    office: "Châu Thành",
+    age: 17,
+    startdate: "11/5/2020",
+  },
+  {
+    name: "Haselgrove",
+    position: "Product Engineer",
+    office: "Barusuda",
+    age: 93,
+    startdate: "1/8/2021",
+  },
+  {
+    name: "Jell",
+    position: "Senior Sales Associate",
+    office: "Gjilan",
+    age: 41,
+    startdate: "11/29/2020",
+  },
+  {
+    name: "Kemer",
+    position: "Junior Executive",
+    office: "Próchoma",
+    age: 52,
+    startdate: "5/30/2020",
+  },
+  {
+    name: "Smardon",
+    position: "Project Manager",
+    office: "Donggaohuang",
+    age: 71,
+    startdate: "8/15/2020",
+  },
+];
+
+var resultSearch = [];
+
+var textdes = document.getElementById('text-des');
+
+const pagList = document.querySelector(".pagination");
+
+function renderTable(data, currentPage, size = 10) {
+  let arrayList = [];
+  arrayList = data.slice().splice(currentPage * size, size);
+  document.getElementById("tbody").innerHTML = arrayList
+    .map((p) => {
+      return `<tr>
+      <td "name">${p.name}</td>
+      <td id = "position">${p.position}</td>
+      <td id = "office">${p.office}</td>
+      <td id ="age">${p.age}</td>
+      <td id ="start-date">${p.startdate}</td>
+    </tr>`;
+    })
+    .join("");
+    let start = currentPage * size;
+    let end = start + size < data.length ? start + size : data.length;
+    textDesShow(start,end,data);
+}
+
+function renderPagination(data, rowsPerPage = 10) {
+  pagList.innerHTML = "";
+  for (let i = 0; i < data.length / rowsPerPage; i++) {
+    pagList.innerHTML += `<button class='pagination__btn'>${i + 1}</button>`;
+  }
+  let btnPag = document.querySelectorAll(".pagination__btn");
+  btnPag.forEach((elem, i) => {
+    elem.addEventListener("click", () => {
+      renderTable(data, i, rowsPerPage);
+      addClassBtn(elem, btnPag);
+    });
+  });
+}
+
+function textDesShow(start,end,arr) {
+  textdes.innerHTML = `Showing ${start} to  ${end} of  ${
+    arr.length
+  } entries`
+}
+
+
+function addClassBtn(btn, prevBtn) {
+  prevBtn.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  btn.classList.add("active");
+}
+
+function setLength() {
+  let rowsPerPage = document.getElementById("length").value;
+  renderPagination(personData, rowsPerPage);
+  renderTable(personData, 0, rowsPerPage);
+}
+
+renderPagination(personData);
+
+renderTable(personData, 0);
+
+var sortTypes = [
+  {
+    name: "a-zName",
+    sort: function sortAZ() {
+      let sortedData = personData.sort((a, b) =>
+        a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+      );
+      let rowsPerPage = document.getElementById("length").value;
+      console.log(sortedData);
+      renderPagination(sortedData, rowsPerPage);
+      renderTable(sortedData, 0, rowsPerPage);
+    },
+  },
+  {
+    name: "a-zPosition",
+    sort: function sortAZ() {
+      let sortedData = personData.sort((a, b) =>
+        a.position > b.position ? 1 : b.position > a.position ? -1 : 0
+      );
+      let rowsPerPage = document.getElementById("length").value;
+      console.log(sortedData);
+      renderPagination(sortedData, rowsPerPage);
+      renderTable(sortedData, 0, rowsPerPage);
+    },
+  },
+  {
+    name: "a-zOffice",
+    sort: function sortAZ() {
+      let sortedData = personData.sort((a, b) =>
+        a.office > b.office ? 1 : b.office > a.office ? -1 : 0
+      );
+      let rowsPerPage = document.getElementById("length").value;
+      console.log(sortedData);
+      renderPagination(sortedData, rowsPerPage);
+      renderTable(sortedData, 0, rowsPerPage);
+    },
+  },
+  {
+    name: "a-zAge",
+    sort: function sortAZ() {
+      let sortedData = personData.sort((a, b) =>
+        a.age > b.age ? 1 : b.age > a.age ? -1 : 0
+      );
+      let rowsPerPage = document.getElementById("length").value;
+      console.log(sortedData);
+      renderPagination(sortedData, rowsPerPage);
+      renderTable(sortedData, 0, rowsPerPage);
+    },
+  },
+  {
+    name: "a-zDate",
+    sort: function sortAZ() {
+      let sortedData = personData.sort(function (a, b) {
+        return new Date(b.startdate) - new Date(a.startdate);
+      });
+      let rowsPerPage = document.getElementById("length").value;
+      console.log(sortedData);
+      renderPagination(sortedData, rowsPerPage);
+      renderTable(sortedData, 0, rowsPerPage);
+    },
+  },
+];
+document
+  .getElementById("name-sort")
+  .addEventListener("click", sortTypes[0].sort);
+document
+  .getElementById("position-sort")
+  .addEventListener("click", sortTypes[1].sort);
+document
+  .getElementById("office-sort")
+  .addEventListener("click", sortTypes[2].sort);
+document
+  .getElementById("age-sort")
+  .addEventListener("click", sortTypes[3].sort);
+document
+  .getElementById("date-sort")
+  .addEventListener("click", sortTypes[4].sort);
+
+
+//search
+const checkItem = (item, searchText) => {
+  return (item.name.toLowerCase().includes(searchText) ||
+      item.position.toLowerCase().includes(searchText) ||
+      item.office.toLowerCase().includes(searchText) ||
+      item.age.toString().includes(searchText)) ? true : false
+}
+const filteredArr = (searchText) => {
+  searchText = searchText.toLowerCase();
+  let arrFiltered = personData.filter((item) => {
+      return checkItem(item, searchText)
+  })
+  let end = arrFiltered.length;
+  let start = 1 ;
+  textDesShow(start,end,arrFiltered)
+  renderTable(arrFiltered);
+}
+const mySearch = () => {
+  let input = document.getElementById('myInput').value;
+  filteredArr(input);;
+}
+
